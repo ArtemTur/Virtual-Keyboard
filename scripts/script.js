1bb694a232074a2f10a76e464727598b6e815c4e
ArtemTur <tur_ne@mail.ru>
2023-05-01 21:11:28 +0300

    feat: add animation  1 май 2023 г. 21:11:28

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,10 @@ document.body.onload = addElement;
 
 const h1 = document.createElement("h1");
 const textArea = document.createElement("input");
+const buttons = document.querySelectorAll(".button");
 const keyboard = document.createElement("ul");
 const keys = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
   "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", 'Del', "CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter", "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "&#8260;", "&#9650;", "Shift", "Ctrl", "Win", "Alt", "", "Alt", "&#9668;", "&#9660;", "&#9658;", "Ctrl"];
-
-
-
 
 
 function addElement() {
@@ -23,21 +21,22 @@ function addElement() {
     li.innerHTML = keys[i];
     li.classList.add("button");
     keyboard.insertAdjacentElement("beforeend", li)
-    if (keys[i].includes("Tab") || keys[i].includes("Backspace") || keys[i].includes("Del") || keys[i].includes("CapsLock") || keys[i].includes("Alt") || 
-    keys[i].includes("Ctrl") ||keys[i].includes("Enter") || keys[i].includes("Shift") || 
-    keys[i].includes("Win") || keys[i].includes("&#9650;") || keys[i].includes("&#9668;")
-     || keys[i].includes("&#9660;") || keys[i].includes("&#9658;")){
+    if (keys[i].includes("Tab") || keys[i].includes("Backspace") || keys[i].includes("Del") || keys[i].includes("CapsLock") || keys[i].includes("Alt") ||
+      keys[i].includes("Ctrl") || keys[i].includes("Enter") || keys[i].includes("Shift") ||
+      keys[i].includes("Win") || keys[i].includes("&#9650;") || keys[i].includes("&#9668;")
+      || keys[i].includes("&#9660;") || keys[i].includes("&#9658;")) {
       li.classList.add("big-button")
     };
-    if(keys[i].length === 0){
+    if (keys[i].length === 0) {
       li.classList.add("space")
     };
-    if (keys[i].includes("Backspace") || keys[i].includes("CapsLock") || keys[i].includes("Enter") || 
-    keys[i].includes("Shift")){
+    if (keys[i].includes("Backspace") || keys[i].includes("CapsLock") || keys[i].includes("Enter") ||
+      keys[i].includes("Shift")) {
       li.classList.add("long")
     }
   };
-
 };
 
- 
+ buttons.forEach(n => n.addEventListener('click', () => {
+  document.querySelector(".textarea").innerHTML = buttons.textArea;
+ }))
