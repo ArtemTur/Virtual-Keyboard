61a8f9216cfa06c964f1695b0ba6ad45c7299a5a
ArtemTur <tur_ne@mail.ru>
2023-05-02 16:51:15 +0300

    feat: add some keyboard  2 май 2023 г. 16:51:15

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 document.body.onload = addElement;
 
+
 const h1 = document.createElement("h1");
-const textArea = document.createElement("input");
+const textArea = document.createElement("textarea");
 const buttons = document.querySelectorAll(".button");
 const keyboard = document.createElement("ul");
 const keys = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
@@ -37,12 +38,33 @@ function addElement() {
   };
 };
 
-  
-document.querySelectorAll("li").forEach(li => {li.addEventListener('click', () => {
-   
-    document.querySelector(".textarea").value += li.textContent;
- })})
 
- 
- 
- 
+function liveKeyboard() {
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+      textArea.value += "\n";
+    } else if (e.key === "Space") {
+      textArea.value += " ";
+    } else if (e.key === "Backspace") {
+      textArea.value = textArea.value.slice(0, -1);
+    } else if (e.key === "Tab") {
+      textArea.value += "   ";
+    } else if (e.key === "ArrowRight") {
+      const cursorPosition = textArea.selectionStart;
+      textArea.setSelectionRange(cursorPosition + 1, cursorPosition + 0);
+    } else if (e.key === "ArrowLeft") {
+      const cursorPosition = textArea.selectionStart;
+      textArea.setSelectionRange(cursorPosition - 1, cursorPosition - 0);
+    } else if (e.key === "ArrowDown") {
+      textArea.setSelectionRange(textArea.value.length, textArea.value.length);
+    } else if (e.key === "ArrowUp") {
+      textArea.setSelectionRange(0, 0);
+    } else if (e.key === "CapsLock") {
+       li.includes("CapsLock").classList.add("active");
+    } else {
+      textArea.value += e.key;
+    }
+  });
+};
+
+liveKeyboard();
